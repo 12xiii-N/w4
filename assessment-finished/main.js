c5b6e1a8656d6a429303f9e29440f604c71d2f86
12xiii-N <119559679+12xiii-N@users.noreply.github.com>
2023-10-12 10:45:09 +0900

    Update main.js

--- assessment-finished/main.js
+++ assessment-finished/main.js
@@ -7,10 +7,10 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-const storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
-const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
-const insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
-const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
+const storyText = '外は50度だったので :insertx: は歩いて行った。 :inserty: に着いたとき、しばらく硬直ていたところ、 :insertz:  ジスは全てを知ってしまったが、特に驚きやしなかった — :insertx: 重さ3000kgで、めちゃくちゃ寒い日だった。';
+const insertX = ['ジェニ', 'ロゼ', 'リサ'];
+const insertY = ['ユニバ', 'ディズニーランド', '国会議事堂'];
+const insertZ = ['地面が崩れ始めていた', '混沌としていた', '雪が溶けていた'];
 
 randomize.addEventListener('click', result);
 
@@ -28,7 +28,7 @@ function result() {
 
   if (customName.value !== '') {
     const name = customName.value;
-    newStory = newStory.replace('Bob', name);
+    newStory = newStory.replace('ジス', name);
   }
 
   if (document.getElementById("uk").checked) {
